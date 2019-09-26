be2a8391298a3eeead0b57bf2040d60f29150527
pinball24 <pinball101962@gmail.com>
2019-09-25 23:35:53 -0500

    tests

---
+++ src/List.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+import renderer from 'react-test-renderer';
+
+
+it('renders without crashing', () => {
+    const section = document.createElement('section');
+    
+    ReactDOM.render(<List />, section);
+
+    ReactDOM.unmountComponentAtNode(section);
+});
+
+it('renders UI as expected', () => {
+    const tree = renderer
+        .create(<List />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
